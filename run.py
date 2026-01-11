e825af7a723cf55300451724a0a2f6e223fd5e67
Polonez1 <darjus.vasiukevic@gmail.com>
2026-01-11 16:00:52 +0200

    ,

---
+++ run.py
@@ -0,0 +1,18 @@
+import click
+import scraper_links
+import scraper_base
+
+
+@click.command()
+@click.option("--search_by", prompt="Search query", help="The search query to scrape.")
+@click.option("--page_from", default=1, help="The starting page number.")
+@click.option("--page_count", default=10, help="The number of pages to scrape.")
+def main(search_by, page_from, page_count):
+    """Main function to run the scraper."""
+    scraper_base.google_search_scrape(
+        search_by=search_by, page_from=page_from, page_count=page_count
+    )
+
+
+if __name__ == "__main__":
+    main(search_by="homeopatija", page_from=1, page_count=1)
--- scraper_base.py
+++ scraper_base.py
@@ -11,30 +11,39 @@ token = login.token
 # patrykkonst123A#
 
 
-search_by = "homeopatija"
+def google_search_scrape(search_by: str, page_from: int = 1, page_count: int = 10):
+    url = "https://scraper-api.decodo.com/v2/scrape"
 
-url = "https://scraper-api.decodo.com/v2/scrape"
+    payload = {
+        "target": "google_search",
+        "query": search_by,
+        "headless": "html",
+        "page_from": page_from,
+        "google_results_language": "en",
+        "parse": True,
+        "page_count": page_count,
+    }
 
-payload = {
-    "target": "google_search",
-    "query": "homeopatija",
-    "headless": "html",
-    "page_from": "1",
-    "google_results_language": "en",
-    "parse": True,
-    "page_count": 10,
-}
+    headers = {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "authorization": f"Basic {token}",
+    }
 
-headers = {
-    "accept": "application/json",
-    "content-type": "application/json",
-    "authorization": f"Basic {token}",
-}
+    print(f"Scraping Google for '{search_by}'")
+    response = requests.post(url, json=payload, headers=headers)
+    data = response.json()
 
-response = requests.post(url, json=payload, headers=headers)
-data = response.json()
+    print("Scraping completed. Saving response to file.")
+    with open(
+        f"./output/response_{search_by}_{page_from}_{page_from}.json",
+        "w",
+        encoding="utf-8",
+    ) as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
 
-with open("response_10.json", "w", encoding="utf-8") as f:
-    json.dump(data, f, ensure_ascii=False, indent=2)
+    print("Response saved successfully.")
 
-print("Done")
+
+if __name__ == "__main__":
+    google_search_scrape("test", 1, 1)
